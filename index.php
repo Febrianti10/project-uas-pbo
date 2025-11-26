aae5370defc94921979af43ed338503fec582598
erenorren <regishashrn07@gmail.com>
2025-11-27 04:37:10 +0700

    upd

--- index.php
+++ index.php
@@ -1,47 +1,5 @@
 <?php
 
-// session_start();
-
-// $page = $_GET['page'] ?? 'dashboard';
-
-// switch ($page) {
-//     case 'dashboard':
-//         include 'views/dashboard.php';
-//         break;
-
-//     case 'transaksi':
-//         include 'views/transaksi.php';
-//         break;
-
-//     case 'layanan':
-//         include 'views/layanan.php';
-//         break;
-
-//     case 'hewan':
-//         include 'views/hewan.php';
-//         break;
-
-//     case 'pemilik':         
-//         include 'views/pelanggan.php';
-//         break;
-
-//     case 'laporan':
-//         include 'views/laporan.php';
-//         break;
-
-//     case 'login':
-//         include 'views/login.php';
-//         break;
-
-//     case 'logout':
-//         session_destroy();
-//         header('Location: index.php?page=login');
-//         exit;
-
-//     default:
-//         include 'views/404.php';   
-//         break;
-
 // index.php - Entry Point Gabungan (Frontend + Backend)
 
 
@@ -151,6 +109,5 @@ switch ($page) {
         include 'views/404.php';
         break;
 }
-?>
 
 
